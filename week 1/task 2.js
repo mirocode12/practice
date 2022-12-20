3391b5ff32e6526a3d4af57598cfa7b95638d2ea
mirocode12 <miro.mandic.12@hotmail.com>
2022-12-20 16:49:56 +0000

    third task 2 finished

--- week 1/task 2.js
+++ week 1/task 2.js
@@ -20,3 +20,16 @@ function capitalizeWords(str){
 console.log(capitalizeWords("the quick brown fox"));
 console.log(capitalizeWords("this is a test"));
 console.log(capitalizeWords("flying parrot is lost"));
+
+/* Write a function that takes a string as an argument and returns string with all digits removed */
+
+function removeDigits(str){
+        return str.replace(/\d/g, "");
+}
+
+console.log(removeDigits("My 3N4am32e4 i244s99 9Jo99h3n D00o5e"));
+console.log(removeDigits("T5h81e 8Qu04i312c75k 8Br0o312v66n Fo993x 4J39u7mp76s"));
+
+
+
+
