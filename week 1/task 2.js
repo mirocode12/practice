bf24f6e9e52e962910f667b2e1b99b57654548bb
mirocode12 <miro.mandic.12@hotmail.com>
2022-12-20 16:40:36 +0000

    second task 2 finished

--- week 1/task 2.js
+++ week 1/task 2.js
@@ -7,3 +7,16 @@ function removeVowels(str){
 console.log(removeVowels("Hello world!"));
 console.log(removeVowels("This is a test"));
 console.log(removeVowels("aeiouAEIOU"));
+
+/* Write a function that takes a string as an argument and returns the string with the first letter of each word capitalized */
+
+function capitalizeWords(str){
+    const splitString = str.split(" ");
+    return splitString.reduce((acc, word) =>
+        acc + word.slice(0,1).toUpperCase() + word.slice(1) + " "
+    ,"");
+}
+
+console.log(capitalizeWords("the quick brown fox"));
+console.log(capitalizeWords("this is a test"));
+console.log(capitalizeWords("flying parrot is lost"));
