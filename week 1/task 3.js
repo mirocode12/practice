c5f4d971ee8325b801208ffd6d6f5129f89d7087
mirocode12 <miro.mandic.12@hotmail.com>
2022-12-21 16:32:45 +0000

    second task 3 finished

--- week 1/task 3.js
+++ week 1/task 3.js
@@ -7,3 +7,22 @@ function sumArray(arr){
 console.log(sumArray([1,2,3,4]));
 console.log(sumArray([10,20,30]));
 console.log(sumArray([-1,-2,-3,-4]));
+
+
+/** Write a function called `isPalindrome` that takes in a string and returns 'true' if string
+ * is a palindrome and false if its not
+ */
+
+function isPalindrome(str){
+    str = str.replace(/[^a-z0-9]/gi,'').toLowerCase();
+    for(i = 0; i < str.length / 2; i++){
+        if(str[i] !== str[str.length - i - 1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+isPalindrome("racecar");    // true
+isPalindrome("hello");    // false
+isPalindrome("A man a plan a canal Panama");    // true
