0cdbbf610ae5b1884976b30d812f3954c6caf6ac
mirocode12 <miro.mandic.12@hotmail.com>
2022-12-22 17:56:14 +0000

    second task 4 finished

--- week 1/task 4.js
+++ week 1/task 4.js
@@ -20,3 +20,36 @@ function sortWords(arr) {
 }
 
 sortWords(["aaple", "mango", "banana", "apple", "cherry"]);
+
+/** Write a function that takes in a string and returns and object with the following properties:
+ * `numVowels`: the number of viwels in the string
+ * `numConsonants`: the number of consonants in the string
+ * `longestWord`: the longest word in the string
+ */
+
+function analyzeString(str) {
+  return (
+    numberOfVowels(str) + " " + numberOfConsonants(str) + " " + longestWord(str)
+  );
+}
+
+function numberOfVowels(str) {
+  return str.match(/[aeiou]/gi, "").length;
+}
+
+function numberOfConsonants(str) {
+  return str.match(/[bcdfghjklmnpqrstvwxyz]/gi, "").length;
+}
+
+function longestWord(str) {
+  let words = str.split(' ');
+  let longestWord = words[0];
+  words.forEach((element) => {
+    if(element.length > longestWord.length){
+        longestWord = element;
+    }
+  });
+  return longestWord;
+}
+
+analyzeString("this is a test.");
