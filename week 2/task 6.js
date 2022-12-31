2efba0dbf6c3d30c61b5936d9a82cf4232f92561
mirocode12 <miro.mandic.12@hotmail.com>
2022-12-31 20:26:59 +0000

    second task 6 finished

--- week 2/task 6.js
+++ week 2/task 6.js
@@ -21,3 +21,14 @@ function sumOfTwo(a, b, t) {
 }
 
 sumOfTwo([1, 2, 3], [4, 5, 6], 7);
+
+/** Write a function called flattenArray that takes an array of arrays as an argument
+ * and returns a new array containing all the elements of the original array, 
+ * flattened into a single array. 
+ * */
+
+function flattenArray(arr){
+    return arr.flat(Infinity);
+}
+
+flattenArray([[1, 2], [3, 4, [5, 6]]])
