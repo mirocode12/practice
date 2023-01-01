906d33b5dd0edc399bd785d13e5be2c57c7bb0c8
mirocode12 <miro.mandic.12@hotmail.com>
2023-01-01 23:12:51 +0000

    second task 7 finished

--- week 2/task 7.js
+++ week 2/task 7.js
@@ -11,4 +11,27 @@ function compareValues(obj1, obj2) {
 compareValues({ age: 25, beard: true }, { age: 25, hair: "long", beard: true }); // false
 compareValues({ age: 25, hair: "long", beard: true }, { age: 25, beard: true }); // true
 
+/** Given a pattern and a string s, find if s follows the same pattern.
 
+Here follow means a full match, such that there is a bijection between a letter in pattern and 
+a non-empty word in s.*/
+
+function wordPattern(pattern, s) {
+  let patternHolder = {};
+  s = s.split(" ");
+  if (pattern.length === s.length) {
+    for (i = 0; i < s.length; i++) {
+      if (patternHolder[pattern[i]] && patternHolder[pattern[i]] !== s[i]) {
+        return false;
+      }else{
+        patternHolder[pattern[i]] = s[i];
+      }
+    }
+    return true;
+  }else{
+    return false;
+  }
+}
+
+wordPattern("abba", "cat car car cat") // true
+wordPattern("abba", "cat car cat cat") // false
